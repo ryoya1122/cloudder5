48a18397208d4a30b884e82041a1ca76fe3c7782
EC2 Default User <ec2-user@ip-172-31-20-34.us-east-2.compute.internal>
2019-03-15 19:59:28 +0000

    3

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,15 +5,10 @@ class User < ActiveRecord::Base
     uid = auth[:uid]
     nickname = auth[:info][:nickname]
     image_url = auth[:info][:image]
-    token = auth[:credentials][:token]
-    stoken = auth[:credentials][:secret]
-    
     
     self.find_or_create_by(provider: provider, uid: uid) do |user|
       user.nickname = nickname
       user.image_url = image_url
-      user.token = token
-      user.stoken = stoken
     end
   end
 end
