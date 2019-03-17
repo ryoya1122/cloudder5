385957be6325cc254487c4bad7b5f495cdbf3411
EC2 Default User <ec2-user@ip-172-31-20-34.us-east-2.compute.internal>
2019-03-17 14:26:44 +0000

    deploy heroku

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require popper
+//= require jquery3
 //= require bootstrap-sprockets
 //= require rails-ujs
 //= require activestorage
